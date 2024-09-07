a8ab197a1ab9de9515a8df69e5fc995c25454b11
Evgeniya-Kr <jenya-kr@yandex.ru>
2024-09-07 22:52:33 +0700

    Add files via upload

--- Функция с параметром Тетерева ЕН.py
+++ Функция с параметром Тетерева ЕН.py
@@ -5,7 +5,7 @@ def get_matrix(n, m, value):
         matrix.append(line_)
         for j in range(m):
             line_.append(value)
-        return matrix
+    return matrix
 result1 = get_matrix(2, 2, 10)
 result2 = get_matrix(3, 5, 42)
 result3 = get_matrix(4, 2, 13)
